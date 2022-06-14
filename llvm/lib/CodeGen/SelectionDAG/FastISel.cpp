593ede24b58522942219d217ce53cafc05fe7c64
Scott Linder <Scott.Linder@amd.com>
2022-06-14 20:45:27 +0000

    [HeterogeneousDWARF] Implement RegAllocFast
    As fast regalloc is fundamentally basic-block local, and the new debug
    information at this stage is fundamentally defined across basic-blocks,
    there is more involved with maintaining the new debug info than is
    required for the existing debug info. Further complicating this is
    that fast regalloc does not visit blocks in any particular order,
    meaning the final debug info for any block cannot actually be known
    until all other blocks are visited.
    
    Implement maximal debug info for cases where a virtual register is known
    spilled at the time the debug intrinsic is visited. Implement
    conservatively correct debug info when the virtual register is not (yet)
    spilled. In this conservative mode, record the inserted debug info and
    the input debug info, and either replace it with complete info (if the
    virtual register is later spilled) or retain the conservatively correct
    info.
    
    Change-Id: Iedc56604ec23dbaa447562478cdf81bb76fe1867

--- llvm/lib/CodeGen/SelectionDAG/FastISel.cpp
+++ llvm/lib/CodeGen/SelectionDAG/FastISel.cpp
@@ -1370,17 +1370,17 @@ bool FastISel::selectIntrinsicCall(const IntrinsicInst *II) {
     const Value *Referrer = DDI.getReferrer();
     assert(Referrer);
     if (const auto *UV = dyn_cast<UndefValue>(Referrer)) {
-      BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, DbgLoc,
+      BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, MIMD,
               TII.get(TargetOpcode::DBG_DEF))
           .addMetadata(DDI.getLifetime())
           .addReg(Register());
     } else if (const auto *CI = dyn_cast<ConstantInt>(Referrer)) {
-      BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, DbgLoc,
+      BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, MIMD,
               TII.get(TargetOpcode::DBG_DEF))
           .addMetadata(DDI.getLifetime())
           .addCImm(CI);
     } else if (auto *CFP = dyn_cast<ConstantFP>(Referrer)) {
-      BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, DbgLoc,
+      BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, MIMD,
               TII.get(TargetOpcode::DBG_DEF))
           .addMetadata(DDI.getLifetime())
           .addFPImm(CFP);
@@ -1388,7 +1388,7 @@ bool FastISel::selectIntrinsicCall(const IntrinsicInst *II) {
       auto SI = FuncInfo.StaticAllocaMap.find(AI);
       if (SI != FuncInfo.StaticAllocaMap.end()) {
         DILifetime *Lifetime = DDI.getLifetime();
-        BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, DbgLoc,
+        BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, MIMD,
                 TII.get(TargetOpcode::DBG_DEF))
             .addMetadata(Lifetime)
             .addFrameIndex(SI->second);
@@ -1404,7 +1404,7 @@ bool FastISel::selectIntrinsicCall(const IntrinsicInst *II) {
         LLVM_DEBUG(dbgs() << "Dropping debug info for alloca " << DDI << "\n");
       }
     } else if (Register Reg = lookUpRegForValue(Referrer)) {
-      BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, DbgLoc,
+      BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, MIMD,
               TII.get(TargetOpcode::DBG_DEF))
           .addMetadata(DDI.getLifetime())
           .addReg(Reg);
@@ -1415,7 +1415,7 @@ bool FastISel::selectIntrinsicCall(const IntrinsicInst *II) {
   }
   case Intrinsic::dbg_kill: {
     const DbgKillInst &DKI = *cast<DbgKillInst>(II);
-    BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, DbgLoc,
+    BuildMI(*FuncInfo.MBB, FuncInfo.InsertPt, MIMD,
             TII.get(TargetOpcode::DBG_KILL))
         .addMetadata(DKI.getLifetime());
     return true;
